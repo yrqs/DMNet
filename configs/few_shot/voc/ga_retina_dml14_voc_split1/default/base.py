7227ad9d7cc5403a30c02677795014ede3eb6977
yrqs <yrqs1234@foxmail.com>
2022-03-11 02:12:51 +0800

    20220311

--- configs/few_shot/voc/ga_retina_dml14_voc_split1/default/base.py
+++ configs/few_shot/voc/ga_retina_dml14_voc_split1/default/base.py
@@ -123,8 +123,8 @@ img_norm_cfg = dict(
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    # dict(type='Expand'),
-    # dict(type='MinIoURandomCrop'),
+    dict(type='Expand'),
+    dict(type='MinIoURandomCrop'),
     dict(type='Resize', img_scale=(1000, 600), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
--- tools/dist_train1.sh
+++ tools/dist_train1.sh
@@ -9,16 +9,16 @@ WORK_DIR_BASE='work_dirs/'$MODEL_NAME'/'$PARAMETER'/'
 PORT=${PORT:-37500}
 PYTHON=${PYTHON:-"python"}
 
-#CONFIG=$CONFIG_PATH'base.py'
-#GPUS=8
-#OMP_NUM_THREADS=2 \
-#CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 $PYTHON -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-#    $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:3} \
-#    --validate \
-#    --work_dir $WORK_DIR_BASE'base' && sleep 1s
+CONFIG=$CONFIG_PATH'base.py'
+GPUS=4
+OMP_NUM_THREADS=2 \
+CUDA_VISIBLE_DEVICES=0,1,2,3 $PYTHON -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+    $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:3} \
+    --validate \
+    --work_dir $WORK_DIR_BASE'base' && sleep 1s
 
-GPU_ID=0,1,2,3,4,5,6,7
-GPUS=8
+GPU_ID=0,1,2,3
+GPUS=4
 CONFIG=$CONFIG_PATH'finetuneD.py'
 
 for i in {1,2,3,5,10}
